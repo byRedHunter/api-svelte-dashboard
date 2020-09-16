3f6a1971a35eb4a3ab16e7f2aaa735a513e3f79d
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-09-16 17:39:17 -0500

    Fix warnings

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,13 +2,16 @@ const { Router } = require('express')
 const router = Router()
 
 // db
-const { dashboard, overview } = require('../db.json')
-
-const data = { dashboard, overview }
+const overview = require('../overview.json')
+const dashboard = require('../dashboard.json')
 
 // get data
 router.get('/', (req, res) => {
-	res.json(data)
+	res.json(dashboard)
+})
+
+router.get('/overview', (req, res) => {
+	res.json(overview)
 })
 
 module.exports = router
